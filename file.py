20f1c5d2283f0018899156304c00d465334745e1
student <ykuleznev@fmschool72.ru>
2021-12-10 17:00:14 +0500

    101 комп

--- file.py
+++ file.py
@@ -1,5 +1,23 @@
 # это очень важный проект на github, откройте другую папку
-print("Hello there")
-s = input()
-if s.lower() == "general kenobi":
-    print("Ok, boomer")
+import pygame
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render("Hello, Pygame!", True, pygame.Color("#ab0baa"))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    draw(screen)
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pygame.display.flip()
+    pygame.quit()
