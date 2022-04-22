5ee1cf5a93354f0c466a37f4c6cab158c4422219
student <ykuleznev@fmschool72.ru>
2022-04-22 17:47:24 +0500

    комп в 101 sqlalchemy

--- file2.py
+++ file2.py
@@ -1,20 +1,14 @@
-class Point:
-    def __init__(self, name, x, y):
-        self.x = x
-        self.y = y
-        self.name = name
-
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
-
-    def get_coords(self):
-        return tuple([self.x, self.y])
-
-    def __invert__(self):
-        return Point(self.name, self.y, self.x)
-
-    def __str__(self):
-        return self.name + "(" + str(self.x) + ", " + str(self.y) + ")"
+n = int(input())
+i = 2
+m = []
+while i <= int(n ** 0.5) + 2:
+    if n % i == 0:
+        m.append(i)
+        n = n // i
+    else:
+        i = i + 1
+if n > 1:
+    print(n)
+else:
+    m.sort()
+    print("*".join(str(i) for i in m))
