c97b1c1eb2c2766409959abcda4dc46fbc3a372f
student <ykuleznev@fmschool72.ru>
2022-04-22 17:47:24 +0500

    комп в 101 sqlalchemy

--- file2.py
+++ file2.py
@@ -1,20 +1,14 @@
-def f(n):
-    c4 = 0
-    n4 = n
-    while n4:
-        c4 += n4 % 4
-        n4 //= 4
-    c8 = 0
-    n8 = n
-    while n8:
-        c8 += n8 % 8
-        n8 //= 8
-    return c8 == c4
-
-
-ans = 0
-n = 40
-for i in range(2 ** n):
-    if f(i):
-        ans += 1
-print(ans)
+n = int(input())
+i = 2
+m = []
+while i <= int(n ** 0.5) + 2:
+    if n % i == 0:
+        m.append(i)
+        n = n // i
+    else:
+        i = i + 1
+if n > 1:
+    print(n)
+else:
+    m.sort()
+    print("*".join(str(i) for i in m))
